daf013288d85cb96f4c16a8c3dd0d98adb792474
Rutuja412 <rutujakadam4545@gmail.com>
2023-02-10 17:06:22 +0530

    [Rutuja]:Adding Test for Average Fare

--- CabInvoiceTest/UnitTest1.cs
+++ CabInvoiceTest/UnitTest1.cs
@@ -31,5 +31,15 @@ namespace CabInvoiceTest
             InvoiceSummary expectedSummary = new InvoiceSummary(2, 31.0);
             Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
         }
+        [Test]
+        public void Return_Multiple_Rides_AverageFare()
+        {
+            InvoiceGenerator invoice = new InvoiceGenerator(RideType.PREMIUM);
+            Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+
+            InvoiceSummary summary = new InvoiceSummary(2, 55.0);
+            InvoiceSummary expectedSummary = invoice.CalculateFare(rides);
+            Assert.AreEqual(expectedSummary.GetType(), summary.GetType());
+        }
     }
 }
